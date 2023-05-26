5433dd8c4b3012bc016719084bdf3a5e178f8d13
Dima Belov <wassermannsn@gmail.com>
2023-05-26 12:06:24 +0600

    feat: add columns_to_category_children_table

--- database/migrations/2023_05_26_060119_create_category_children_table.php
+++ database/migrations/2023_05_26_060119_create_category_children_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('category_children', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('category_id')->index()->nullable();
             $table->timestamps();
         });
     }
