0d31a7ad2215dadb712633f025fe05115b7af3cf
Dima Belov <wassermannsn@gmail.com>
2023-05-27 15:17:26 +0600

    feat: create product resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
+        $products = Product::all();
+        return ProductResource::collection(
+            $products
+        );
+
 
     }
 }
