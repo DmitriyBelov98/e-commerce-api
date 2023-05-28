9488db17fad9ae775e1dd687b49dcb4500303cc0
Dima Belov <wassermannsn@gmail.com>
2023-05-28 18:51:42 +0600

    refactor: change model name for more clear understanding

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -24,7 +24,7 @@ class Product extends Model
 
     public function variations()
     {
-        return $this->hasMany(Variations::class);
+        return $this->hasMany(ProductVariations::class);
     }
 
 
--- app/Models/ProductVariations.php
+++ app/Models/ProductVariations.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Variations extends Model
+class ProductVariations extends Model
 {
     use HasFactory;
 }
