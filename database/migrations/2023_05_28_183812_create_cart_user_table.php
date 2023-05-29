1f08ad25f928b3af6651a52eb281203b69ead68e
Dima Belov <wassermannsn@gmail.com>
2023-05-29 12:49:51 +0600

    feate(table): add columns to cart_user_table

--- database/migrations/2023_05_28_183812_create_cart_user_table.php
+++ database/migrations/2023_05_28_183812_create_cart_user_table.php
@@ -15,7 +15,11 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id')->index();
             $table->unsignedBigInteger('product_variation_id')->index();
+            $table->unsignedBigInteger('quantity');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('product_variation_id')->references('id')->on('product_variations');
         });
     }
 
