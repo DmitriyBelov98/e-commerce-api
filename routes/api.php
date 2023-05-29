f99dacb06f046d24371dfc37db58e56235354b4a
Dima Belov <wassermannsn@gmail.com>
2023-05-29 13:21:39 +0600

    feat: create auth:sanctum route group for users cart

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
@@ -18,8 +19,13 @@ use Illuminate\Support\Facades\Route;
 */
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('cart', CartController::class);
+
 });
 
 //Route::get('/categories', [CategoryController::class, 'index']);
