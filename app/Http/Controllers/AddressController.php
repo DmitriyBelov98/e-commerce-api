0a1b25b62f713b43356a1522982c30fed0562b09
Dima Belov <wassermannsn@gmail.com>
2023-06-01 16:55:25 +0600

    feat: create store address  method for creating user address

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\AddressResource;
+use App\Models\Address;
 use Illuminate\Http\Request;
 
 class AddressController extends Controller
@@ -10,6 +11,15 @@ class AddressController extends Controller
     public function index(Request $request)
     {
         return AddressResource::collection($request->user()->addresses);
+    }
+
+    public function store(Request $request)
+    {
+        $address = Address::create($request->only(['user_name', 'address', 'city', 'postal_code']));
+
+        $request->user()->addresses()->save($address);
+
+        return new AddressResource($address);
     }
 
 
--- app/Models/Address.php
+++ app/Models/Address.php
@@ -10,7 +10,7 @@ class Address extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name',
+        'user_name',
         'address',
         'city',
         'postal_code'
