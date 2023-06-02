2ce30100cbff5c04e4fc74dd62e1702fce76b26d
Dima Belov <wassermannsn@gmail.com>
2023-06-02 10:28:32 +0600

    feat: create product factory for inserting fake data

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,14 +2,13 @@
 
 namespace Database\Factories;
 
-use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @extends Factory<=Category>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
  */
-class CategoryFactory extends Factory
+class ProductFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -19,9 +18,10 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-
             'name' => $name = $this->faker->unique()->word,
-            'slug' => Str::slug($name)
+            'slug' => Str::slug($name),
+            'description' => $this->faker->sentence(5),
+            'price' => random_int(1,100),
         ];
     }
 }
